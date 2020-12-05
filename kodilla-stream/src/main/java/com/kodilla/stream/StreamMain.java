b9eb6f1cb053174cc1a3c2b08a0a253e728eab9f
Jakub Ciosek <ciosekjakub@wp.pl>
2020-12-05 15:03:51 +0100

    7.1 Task

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,38 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions eith methon references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Text to decorate", text -> text.toUpperCase());
+        poemBeautifier.beautify("Text to decorate", text -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("Text to Decorate", text -> text.toLowerCase());
+        poemBeautifier.beautify("Text to Decorate", text -> text.replaceAll("Text", "TEXT"));
+
+
+    }
+
+
+}
+
+
