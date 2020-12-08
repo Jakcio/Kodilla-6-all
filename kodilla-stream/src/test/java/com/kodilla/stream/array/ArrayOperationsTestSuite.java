0fb35df2ee14a2c5226bd98bb56da2a9c11488bb
Jakub Ciosek <ciosekjakub@wp.pl>
2020-12-08 11:23:17 +0100

    Kodilla 7.5

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,2 +1,30 @@
-package com.kodilla.stream.array;public class ArrayOperationsTestSuite {
+package com.kodilla.stream.array;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+
+    @Test
+    void testGetAverage() {
+        //Given
+
+        int numbers[] = {2,3,4,6,7,8,99,0,0,8,6,54,14,55,33,23,25,66,44,23};
+
+        double expectedAverage = 24;
+
+        //When
+
+
+        double average = ArrayOperations.getAverage(numbers);
+
+        //Then
+
+        assertEquals(average, expectedAverage, 0);
+
+
+    }
+
+
+
 }
